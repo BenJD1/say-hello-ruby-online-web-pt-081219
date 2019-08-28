cb7806d09c16a4e909b1b4f1139aa4232ec160f0
Ben Dunnell <bjdunnell96@gmail.com>
2019-08-28 21:54:56 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,5 @@
-# I call on the method, say_hello,and give it the string "Hello Kent Beck
+# Build your say_hello method here
+def say_hello(name="Ruby Programmer")
+  puts "Hello " + name + "!"
+end
+say_hello("Kent Beck")
